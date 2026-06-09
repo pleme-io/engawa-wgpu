dcf6448f6b1f5402234b9450bf3b0eaba2a9e28f
drzln <luis.d@akeyless.io>
2026-06-08 17:41:57 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   description = "engawa-wgpu — wgpu-backed Dispatcher impl for engawa render graphs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     crate2nix.url = "github:nix-community/crate2nix";
     flake-utils.url = "github:numtide/flake-utils";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
